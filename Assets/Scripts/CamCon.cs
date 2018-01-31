22318e2499a772d6301260976328722f9353aa3f
snivins <vicentejlopezballen@gmail.com>
2018-01-31 19:55:51 +0100

    Initial commit

---
+++ Assets/Scripts/CamCon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CamCon : NetworkBehaviour {
+	public Transform focus;
+	public float camSpeed;
+	public GameObject camara;
+	public GameObject camaraPitch;
+	public GameObject camaraYaw;
+	// Use this for initialization
+	void Start () {
+		camSpeed = 50f;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (focus != null) {
+			float y = camSpeed * Input.GetAxis ("Mouse X");
+			float x = camSpeed * Input.GetAxis ("Mouse Y");
+			camaraYaw.transform.Rotate(new Vector3 (0.0f, y,0.0f), Time.deltaTime * camSpeed,Space.Self);
+			camaraPitch.transform.Rotate(new Vector3 (-x,0.0f, 0.0f), Time.deltaTime * camSpeed,Space.Self);
+			if (Input.GetAxis ("Mouse ScrollWheel") != 0.0f) {
+				float zoom = Input.GetAxis("Mouse ScrollWheel");
+				camara.transform.Translate(new Vector3 (0.0f, 0.0f, zoom) * Time.deltaTime * 1000,Space.Self);
+			}
+		} else {
+			if (GameObject.FindGameObjectWithTag ("Player") != null) {
+				focus = GameObject.FindGameObjectWithTag ("Player").transform;
+				transform.position = focus.position;
+				//transform.SetParent (focus);
+
+
+
+			}
+		}
+
+	}
+}
+
+
+
---
+++ Assets/Scripts/pew.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pew : MonoBehaviour {
+	private LineRenderer laser;
+	public GameObject chispas;
+	// Use this for initialization
+	void Start () {
+		laser = GetComponent<LineRenderer> ();
+		//Vector3[] posIniciales = new Vector3[2]; 
+		//posIniciales[0] = new Vector3 (0f, 0f, 0f);
+		//posIniciales[1] = new Vector3(0f,0f,0f);
+		//laser.SetPositions (posIniciales);
+		GameObject lalilulelo = Instantiate (chispas, transform.position, transform.rotation);
+		chispas = lalilulelo;
+		Destroy (chispas, 3f);
+		chispas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		laser.SetPosition (0, transform.position);	
+		laser.startWidth = Random.Range(0.5f,1.5f);
+		laser.endWidth =  Random.Range(0.5f,1.5f);
+		Ray ray = new Ray (transform.position, transform.forward*500f); //Camera.main.ScreenPointToRay (Input.mousePosition).direction);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 5000f)) {
+			if (hit.rigidbody) {	
+				chispas.SetActive (true);
+				chispas.transform.position = hit.point;
+
+				chispas.transform.rotation = hit.transform.rotation;//Quaternion.Euler( hit.normal);
+			} 
+			laser.SetPosition (1, hit.point);	
+			//Debug.Log (ray.GetPoint (4500f));
+		} else {
+			chispas.SetActive (false);
+			laser.SetPosition (1, transform.forward*5000f);
+		}
+
+
+	}
+}
